280bcba0abe7cb7fe8a302909a51de124d77e7a7
Unnathi4 <119917958+Unnathi4@users.noreply.github.com>
2022-12-15 15:44:11 +0530

    Update test_handler.py

--- Calculator_samp/tests/unit/test_handler.py
+++ Calculator_samp/tests/unit/test_handler.py
@@ -4,6 +4,21 @@ import unittest
 
 from hello_world import app
 
+
+
+
+def lambda_handler(event, context):
+    number1 = event['Number1']
+    number2 = event['Number2']
+    def sum():
+        return number1 + number2
+    def product():
+        return number1 * number2
+    def difference():
+        return abs(number1 - number2)
+    def quotient():
+        return number1 / number2
+
 class TestSum(unittest.TestCase):
 
     def test_add(self):
