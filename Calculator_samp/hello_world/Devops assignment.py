9bd91efe179be2b1d5374460640b4450657ae8d3
Unnathi4 <119917958+Unnathi4@users.noreply.github.com>
2022-12-15 16:53:11 +0530

    changed

--- Calculator_samp/hello_world/Devops assignment.py
+++ Calculator_samp/hello_world/Devops assignment.py
@@ -1,16 +1,21 @@
-import json
 from __future__ import division
+import json
+
 
 
 def lambda_handler(event, context):
     number1 = event['Number1']
     number2 = event['Number2']
-    def sum():
-        return number1 + number2
-    def product():
-        return number1 * number2
-    def difference():
-        return abs(number1 - number2)
-    def quotient():
-        return number1 / number2
+    print(sum(number1,number2))
+    print(product(number1,number2))
+    print(difference(number1,number2))
+    print(quotient(number1,number2))
     
+def sum(number1,number2):
+   return number1 + number2
+def product():
+    return number1 * number2
+def difference():
+    return abs(number1 - number2)
+def quotient():
+    return number1 / number2
