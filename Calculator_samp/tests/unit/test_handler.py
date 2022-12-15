856dd17ecd995901e0256bffcb863f19c3ccf387
Unnathi4 <119917958+Unnathi4@users.noreply.github.com>
2022-12-15 16:50:57 +0530

    updated

--- Calculator_samp/tests/unit/test_handler.py
+++ Calculator_samp/tests/unit/test_handler.py
@@ -2,18 +2,18 @@ import json
 
 import unittest
 
-from hello_world import app
+import app
 
 class TestSum(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(app.sum([2,2]), 4, "Should be 4")
+        self.assertEqual(app.sum(2,2), 4, "Should be 4")
     def test_product(self):
-        self.assertEqual(app.product([3, 2]), 6, "Should be 6")
+        self.assertEqual(app.product(3, 2), 6, "Should be 6")
     def test_sub(self):
-        self.assertEqual(app.difference([4, 1]), 3, "Should be 3")
+        self.assertEqual(app.difference(4, 1), 3, "Should be 3")
     def test_division(self):
-        self.assertEqual(app.quotient([4,2]), 2, "Should be 2")
+        self.assertEqual(app.quotient(4,2), 2, "Should be 2")
 
 if __name__ == '__main__':
     unittest.main()
